9ca20a756ad7ce146021cb45294eac90caef7b28
Minh Nhut Bui <rinrip225@gmail.com>
2023-09-16 22:35:22 +1000

    Remove default UI of vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="nav">
+    <div class="nav_left">
+        <img src="../assets/img/logo.svg" alt="Logo">
+    </div>
+    <div class="nav_right">
+        <div class="nav_right--items">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Projects</a></li>
+            </ul>
+        </div>
+        <div class="nav_right--social">
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+        <button class="nav_right--button">Let's Connect</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  
+</style>
