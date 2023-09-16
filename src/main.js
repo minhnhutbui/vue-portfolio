eab31e5041dde3b53cbf58545a318bf58daa8b47
Minh Nhut Bui <rinrip225@gmail.com>
2023-09-17 09:44:30 +1000

    Import and use fontawesome branding icon

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import boostrap from 'bootstrap/dist/js/bootstrap.js'
+import "bootstrap/dist/css/bootstrap.css";
+import boostrap from "bootstrap/dist/js/bootstrap.js";
 
-createApp(App).use(boostrap).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons"
+
+library.add(fas, fab);
+
+createApp(App)
+  .use(boostrap)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
