e0b36851dffc5659a271ce56d1252c1b4984cb8d
Minh Nhut Bui <rinrip225@gmail.com>
2023-09-17 21:35:22 +1000

    Separate App css into a file

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
-  <NavBar />
+    <NavBar />
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue';
+import NavBar from "./components/NavBar.vue";
 export default {
-  name: 'App',
-  components: {
-    NavBar
-  }
-}
+    name: "App",
+    components: {
+        NavBar,
+    },
+};
 </script>
 
-<style lang="scss">
-body {
-  background-color: #121212 !important;
-  color: #fff !important;
-}
+<style>
+@import "./App.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -12,6 +12,6 @@ import { fab } from "@fortawesome/free-brands-svg-icons"
 library.add(fas, fab);
 
 createApp(App)
-  .use(boostrap)
-  .component("font-awesome-icon", FontAwesomeIcon)
-  .mount("#app");
+    .use(boostrap)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount("#app");
