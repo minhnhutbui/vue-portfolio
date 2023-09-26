892135e8070ffdc88dd536d94d5d982672da105d
Minh Nhut Bui <rinrip225@gmail.com>
2023-09-26 23:41:08 +1000

    Install yaml loader and sass loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('yaml')
+        .test(/\.ya?ml?$/)
+        .use('yaml-loader')
+          .loader('yaml-loader')
+          .end()
+  }
 })
+
+
+
+
