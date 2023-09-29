fef3a0b794df54907dfc379199e73256bff24439
Minh Nhut Bui <rinrip225@gmail.com>
2023-09-29 11:54:53 +1000

    Implement navigation bar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,34 +1,130 @@
 <template>
-  <div id="nav">
-    <div class="nav_left">
-        <img src="../assets/img/logo.svg" alt="Logo">
-    </div>
-    <div class="nav_right">
-        <div class="nav_right--items">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Skills</a></li>
-                <li><a href="#">Projects</a></li>
-            </ul>
-        </div>
-        <div class="nav_right--social">
-            <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-            </ul>
-        </div>
-        <button class="nav_right--button">Let's Connect</button>
-    </div>
-  </div>
+	<div id="nav" class="grid wide">
+		<div class="nav_left">
+			<img src="../assets/img/logo.svg" alt="Logo" />
+		</div>
+		<div class="nav_right">
+			<div class="nav_right--items">
+				<ul>
+					<li v-for="item in items" :key="item.name">
+						<a href="#">{{ item.name }}</a>
+					</li>
+				</ul>
+			</div>
+			<div class="nav_right--socials">
+				<ul>
+					<li v-for="social in socials" :key="social.name">
+						<a href="#">
+							<font-awesome-icon :icon="social.icon_classes" />
+						</a>
+					</li>
+				</ul>
+			</div>
+			<button
+				v-for="button in buttons"
+				:key="button.name"
+				class="nav_right--button"
+			>
+				{{ button.text }}
+			</button>
+		</div>
+	</div>
 </template>
 
 <script>
+import { ref } from "vue";
+import navbar from "../data/nav_bar.yml";
+
 export default {
-  
-}
+	setup() {
+		const items = ref(navbar.data.items);
+		const socials = ref(navbar.data.socials);
+		const buttons = ref(navbar.data.buttons);
+		return { items, socials, buttons };
+	},
+};
 </script>
 
-<style scoped>
-  
+<style lang="scss">
+#nav {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 20px 0;
+	.nav_right {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 50%;
+		.nav_right--items {
+			ul {
+				li {
+					a {
+						padding: 10px 20px;
+						color: var(--grey);
+					}
+				}
+			}
+		}
+		.nav_right--socials {
+			ul {
+				li {
+					padding: 10px;
+					a {
+						border-radius: 50%;
+						padding: 10px;
+						width: 42px;
+						height: 42px;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						transition: box-shadow 0.3s;
+						&:hover {
+							box-shadow: 0 0 0 10px rgba(255, 255, 255, 0.4);
+							color: #fff;
+						}
+					}
+					a:has(.fa-facebook-f) {
+						background: linear-gradient(#2599fe, #2a64dd);
+					}
+					a:has(.fa-instagram) {
+						background: linear-gradient(
+							120deg,
+							#731dfc 0,
+							#f726cb 40%,
+							#f9904c 60%
+						);
+					}
+					a:has(.fa-linkedin-in) {
+						background: #1650b6;
+					}
+				}
+			}
+		}
+	}
+	.nav_right--button {
+		background-color: transparent;
+		color: var(--white);
+		border: 2px solid var(--grey);
+		height: 100%;
+		padding: 20px;
+		border-radius: 50px;
+		-webkit-transition: color 0.3s, background-color 0.3s;
+		-moz-transition: color 0.3s, background-color 0.3s;
+		transition: color 0.3s, background-color 0.3s;
+		&:hover {
+			color: var(--black);
+			background-color: var(--white);
+		}
+	}
+	ul {
+		display: flex;
+		li {
+			a {
+				display: block;
+				color: var(--white);
+			}
+		}
+	}
+}
 </style>
