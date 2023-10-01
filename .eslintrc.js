b9c61a293179d5c6fec0dc46ff2fc4cc6f651030
Minh Nhut Bui <rinrip225@gmail.com>
2023-10-01 12:14:32 +1100

    Add vue type writer component

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
         // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
     ],
     rules: {
-        "vue/multi-word-component-names": "off"
+        "vue/multi-word-component-names": "off",
+        "vue/html-indent": "off"
     }
 }
