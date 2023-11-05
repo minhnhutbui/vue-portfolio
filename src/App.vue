d94d7b85734118a6fb562fb1b99304081d2da9e2
Minh Nhut Bui <rinrip225@gmail.com>
2023-11-05 12:26:36 +1100

    Implement skills component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
 	<NavBar />
 	<Banner />
+	<Skills />
+	<Projects />
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
 import Banner from "./components/Banner.vue";
+import Skills from "./components/Skills.vue";
+import Projects from "./components/Projects.vue";
 export default {
 	name: "App",
 	components: {
 		NavBar,
 		Banner,
+		Skills,
+		Projects,
 	},
 };
 </script>
---
+++ src/components/Skills.vue
@@ -0,0 +1,58 @@
+<template>
+	<div id="skills">
+		<div class="content grid wide">
+			<div class="content_header row">
+				<h1 class="header">{{ header }}</h1>
+			</div>
+			<div class="content_description row">
+				<p class="col l-6 description">{{ description }}</p>
+			</div>
+			<div class="content_description row">
+				<div class="col l-4 item" v-for="item in items" :key="item.name">
+					<img :src="require(`../assets/img/${item.src}`)" alt="image" />
+					<span>{{ item.name }}</span>
+					<p>{{ item.description }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from "vue";
+import skills from "../data/skills.yml";
+
+export default {
+	setup() {
+		const header = ref(skills.data.header);
+		const description = ref(skills.data.description);
+		const items = ref(skills.data.items);
+		console.log(items.value);
+		return { header, description, items, modules: [Navigation] };
+	},
+};
+</script>
+
+<style lang="scss">
+#skills {
+	.content {
+		.content_description {
+			text-align: center;
+			.item {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				margin-bottom: 100px;
+				padding: 0 50px;
+				img {
+					width: 25%;
+					aspect-ratio: 1;
+				}
+				span {
+					padding: 20px 0;
+				}
+			}
+		}
+	}
+}
+</style>
