3fbd7d03a1dee4d286b29ea2c9bf809239ab818b
Minh Nhut Bui <rinrip225@gmail.com>
2023-11-07 20:29:57 +1100

    Clean up images and start implementation of projects components

---
+++ src/components/Projects.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="projects">
+		<div class="content grid wide">
+			<div class="content_header row">
+				<h1 class="header">Projects</h1>
+			</div>
+			<div class="content_description row">
+				<p class="col l-6 description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s
+				</p>
+			</div>
+			<div class="row">
+				<p class="col l-4 item">
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {},
+};
+</script>
+
+<style lang="scss"></style>
